458e011435f790b114d50ddfb839e1fcfcb138bf
DESKTOP-6SD338J\SuperUser <eoehd1802@naver.com>
2022-10-18 18:01:00 +0900

    구글차트 기능 추가

--- www/admin/graph.php
+++ www/admin/graph.php
@@ -2,10 +2,11 @@
     include_once trim($_SERVER['DOCUMENT_ROOT'])."/include/inc.common.php";
     $time = $_POST['time'];
     $date = $_POST['date'];
-    if($time)
+    $type = $_POST['g_type'];
+    if($type=="line")
     {
       $sql_column="select a.*,ifnull(b.cnt,0)as cnt from
-      (select @curDate := date_sub(@curDate, interval 1 day) as dates from mpr_event_db inner join (select @curDate := $time) A where @curDate > date_add($time, interval -1 week)) a 
+      (select @curDate := date_sub(@curDate, interval 1 day) as dates from mpr_event_db inner join (select @curDate := $time) A where @curDate > date_add($time, interval -1 week)ORDER by dates asc) a 
           left join (select *, DATE_FORMAT(regdate,'%Y-%m-%d')as now,count(regdate)as cnt from mpr_event_db b GROUP by now)b
             on a.dates = b.now";
                           
@@ -19,13 +20,40 @@
       // $data_1 = array();
       echo json_encode($column);
     }
-    if($date)
+    if($type=="bar")
     {
-      $sql="select br_code, DATE_FORMAT(regdate,'%Y-%m-%d')as dates,count(br_code)as cnt FROM `mpr_event_db` where  DATE_FORMAT(regdate,'%Y-%m-%d')=$date group by br_code order by cnt desc";
+      $sql="select br_code, DATE_FORMAT(regdate,'%Y-%m-%d')as dates,count(br_code)as cnt FROM `mpr_event_db` where  DATE_FORMAT(regdate,'%Y-%m-%d')='$date' group by br_code order by cnt desc";
       $result=$DB->query($sql);
       echo json_encode($result);
     }
-    
+
+    if($type=="pie")
+    {
+      $sql="select if(ev_sex='','기타',if(ev_sex='F','여자','남자'))as ev_sex, count(ev_sex) as cnt from mpr_event_db where br_code ='TwTwiN' group by ev_sex";
+      $result=$DB->query($sql);
+      echo json_encode($result);
+    }
+
+    if($type=="rev_bar")
+    {
+      $sql="select
+      (
+          CASE
+              when ev_age BETWEEN 10 and 19 then '10대'
+              when ev_age BETWEEN 20 and 29 then '20대'
+              when ev_age BETWEEN 30 and 39 then '30대'
+              when ev_age BETWEEN 40 and 49 then '40대'
+              when ev_age BETWEEN 50 and 59 then '50대'
+              when ev_age BETWEEN 60 and 69 then '60대'
+              when ev_age BETWEEN 70 and 79 then '70대'
+              when ev_age BETWEEN 80 and 89 then '80대'
+              when ev_age BETWEEN 90 and 99 then '90대'
+              else '범위밖'
+          END) as age_range, count(ev_age) as cnt
+      from mpr_event_db where br_code='TwTwiN' group by age_range";
+      $result=$DB->query($sql);
+      echo json_encode($result);
+    }
     // 2022-10-13  lfsd2 1
     // 2022-10-14  lfsd2 2
     // 2022-10-15  2dc22 1
--- www/admin/index.php
+++ www/admin/index.php
@@ -4,64 +4,98 @@
 ?>
 
 <div class="content-wrapper">
-
+    <!-- Content Header (Page header) -->
     <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>홈 화면</h1>
-                </div>
-            </div>
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>홈 화면</h1>
+          </div>
         </div>
+      </div><!-- /.container-fluid -->
     </section>
-    <div class="card card-success">
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <!-- interactive chart -->
+            <div class="card card-primary card-outline">
               <div class="card-header">
-                <h3 class="card-title">Bar Chart</h3>
-
-                <div class="card-tools">
-                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                    <i class="fas fa-minus"></i>
-                  </button>
-                  <button type="button" class="btn btn-tool" data-card-widget="remove">
-                    <i class="fas fa-times"></i>
-                  </button>
-                </div>
+                <h3 class="card-title">
+                  <i class="far fa-chart-bar"></i>
+                  일간 상위 데이터("2022-10-12" 기준)
+                </h3>
               </div>
               <div class="card-body">
                 <div class="chart">
-                  <canvas id="barChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
-                  <canvas id="barChart2" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
-                </div>
+                    <canvas id="barChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                    <canvas id="barChart2" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                  </div>
               </div>
-              <!-- /.card-body -->
-      </div>
-      <div class="col-md-6">
-            <!-- LINE CHART -->
-            <div class="card card-info">
+            </div>
+          </div>
+        </div>
+        
+          <div class="col-md-6">
+            <!-- Bar chart -->
+            <div class="card card-primary card-outline">
               <div class="card-header">
-                <h3 class="card-title">Line Chart</h3>
-
-                <div class="card-tools">
-                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                    <i class="fas fa-minus"></i>
-                  </button>
-                  <button type="button" class="btn btn-tool" data-card-widget="remove">
-                    <i class="fas fa-times"></i>
-                  </button>
-                </div>
+                <h3 class="card-title">
+                  <i class="far fa-chart-bar"></i>
+                  주간 누적 이벤트 데이터
+                </h3>
               </div>
               <div class="card-body">
                 <div class="chart">
-                  <canvas id="lineChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
-                </div>
+                    <canvas id="lineChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                  </div>
               </div>
-              <!-- /.card-body -->
+              <!-- /.card-body-->
             </div>
             <!-- /.card -->
-      </div>
 
+            <!-- Donut chart -->
+            <div class="card card-primary card-outline">
+              <div class="card-header">
+                <h3 class="card-title">
+                  <i class="far fa-chart-bar"></i>
+                  이벤트 별 성별 비율 데이터 및 연령대 데이터
+                </h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="card-body">
+                <table>
+                  <tr>
+                    <td><div id="piechart_div" style="border: 1px solid #ccc"></div></td>
+                    <td><div id="barchart_div" style="border: 1px solid #ccc"></div></td>
+                  </tr>
+                </table>
+              </div>
+              <!-- /.card-body-->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+<script src="https://cdn.jsdelivr.net/jquery.flot/0.8.3/jquery.flot.min.js"></script>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript" src="/js/flot/jquery.flot.pie.js"></script>
 <script type="text/javascript" src="jscript/graph.js"></script>
 <script>
 window.onload = function(){
@@ -74,10 +108,12 @@ window.onload = function(){
   $.ajax({
             url:"/admin/graph.php",
             type :"post",
-            data:{time:now},
+            data:{time:now,
+                  g_type: "line",
+                date:asd},
             dataType :'json',
               success: function(data){
-                console.log("1"+data);
+                  console.log(data);
                   var tmp = []
                   var tmp_data = []
                   for(var i=0; i<data.length;i++)
@@ -97,27 +133,99 @@ window.onload = function(){
                       pointStrokeColor    : 'rgba(60,141,188,1)',
                       pointHighlightFill  : '#fff',
                       pointHighlightStroke: 'rgba(60,141,188,1)',
-                      data                : [1,2,3,4,5,6]
+                      data                : [10,22,34,15,25,16,28]
+                    }
+                  ]
+                }
+                var areaChartOptions = {
+                  maintainAspectRatio : false,
+                  responsive : true,
+                  legend: {
+                    display: false
+                  },
+                  scales: {
+                    xAxes: [{
+                      gridLines : {
+                        display : false,
+                      }
+                    }],
+                    yAxes: [{
+                      gridLines : {
+                        display : false,
+                      }
+                    }]
+                  }
+                }
+                // Data.datasets[0]['data']=tmp_data;
+                var lineChartCanvas = $('#lineChart').get(0).getContext('2d')
+                var lineChartOptions = $.extend(true, {}, areaChartOptions)
+                var lineChartData = $.extend(true, {}, Data)
+                lineChartData.datasets[0].fill = false;
+                lineChartOptions.datasetFill = false
+
+                var lineChart = new Chart(lineChartCanvas, {
+                  type: 'line',
+                  data: lineChartData,
+                  options: lineChartOptions
+                })
+              },
+          });
+          
+          $.ajax({
+            url:"/admin/graph.php",
+            type :"post",
+            data:{date:asd,
+                  g_type:"bar"},
+            dataType :'json',
+              success: function(data){
+                console.log(data);
+                  var tmp=[]
+                  var tmp_data=[]
+                  for(var i=0; i<data.length; i++)
+                  {
+                    tmp.push(data[i]['br_code']);
+                    tmp_data.push([data[i]['cnt']]);
+                  }
+                  var Data = {
+                  labels  : tmp,
+                  datasets: [
+                    {
+                      label : "이벤트 종류",
+                      backgroundColor     : 'rgba(60,141,188,0.9)',
+                      borderColor         : 'rgba(60,141,188,0.8)',
+                      pointRadius         :  false,
+                      pointColor          : '#3b8bba',
+                      pointStrokeColor    : 'rgba(60,141,188,1)',
+                      pointHighlightFill  : '#fff',
+                      pointHighlightStroke: 'rgba(60,141,188,1)',
+                      data                : [11,23,34,21,56,23]
                     }
                   ]
                 }
                 Data.datasets[0]['data']=tmp_data;
+                // console.log(Math.max(Data.datasets[0]['data']))\
+                var max = Math.max.apply(null,Data.datasets[0]['data']);
+                var min = Math.min.apply(null,Data.datasets[0]['data']);
+                console.log("최댓값" + max)
+                console.log("최솟값" + min)
                 var barChartCanvas = $('#barChart').get(0).getContext('2d')
                 var barChartData = $.extend(true, {}, Data)
+
                 var barChartOptions = {
                   responsive              : true,
                   maintainAspectRatio     : false,
                   datasetFill             : false,
                   scales: {
-                    yAxes:[{
-                      ticks:{
-                        min:0,
-                        max:10,
-                        fontisize:14,
-                      }
-                    }]
+                          yAxes: [{
+                            ticks: {
+                              max: max+5,
+                              // min: min,
+                              beginAtZero: true,
+                              fontSize : 14,
+                            }
+                          }]
+                        }
                   }
-                }
 
                 new Chart(barChartCanvas, {
                   type: 'bar',
@@ -126,60 +234,70 @@ window.onload = function(){
                 })
               },
           });
-          
+
           $.ajax({
             url:"/admin/graph.php",
             type :"post",
-            data:{date:asd},
+            data:{date:asd,
+                  g_type:"pie"},
             dataType :'json',
               success: function(data){
-                console.log("2"+data);
                   var tmp=[]
-                  var tmp_data=[]
+                  // var tmp_data=[]
                   for(var i=0; i<data.length; i++)
                   {
-                    tmp.push(data[i]['dates']);
-                    tmp_data.push([data[i]['cnt']]);
+                    tmp.push([data[i]['ev_sex'],data[i]['cnt']]);
                   }
-                  var Data = {
-                  labels  : tmp,
-                  datasets: [
-                    {
-                      label : "일자별 개수",
-                      backgroundColor     : 'rgba(60,141,188,0.9)',
-                      borderColor         : 'rgba(60,141,188,0.8)',
-                      pointRadius         :  false,
-                      pointColor          : '#3b8bba',
-                      pointStrokeColor    : 'rgba(60,141,188,1)',
-                      pointHighlightFill  : '#fff',
-                      pointHighlightStroke: 'rgba(60,141,188,1)',
-                      data                : [1,2,3,4,5]
+                  google.charts.load('current', {'packages':['corechart']});
+                  google.charts.setOnLoadCallback(drawChart);
+                  function drawChart() {
+                    var data = google.visualization.arrayToDataTable([
+                      ['sex','cnt'],tmp[0],tmp[1],tmp[2]
+                    ]);
+
+                    var piechart_options = {title:'이벤트: TwTwiN(임시 고정값)',
+                       width:380,
+                       height:300};
+                    var piechart = new google.visualization.PieChart(document.getElementById('piechart_div'));
+                    piechart.draw(data, piechart_options);
                     }
-                  ]
-                }
-                var lineChartCanvas = $('#lineChart').get(0).getContext('2d')
-                var lineChartData = $.extend(true, {}, Data)
-                var lineChartOptions = {
-                  responsive              : true,
-                  maintainAspectRatio     : false,
-                  datasetFill             : false,
-                  scales: {
-                    yAxes:[{
-                      ticks:{
-                        min:0,
-                        max:10,
-                        fontisize:14,
-                      }
-                    }]
-                  }
-                }
-                var lineChart = new Chart(lineChartCanvas, {
-                  type: 'line',
-                  data: lineChartData,
-                  options: lineChartOptions
-                })
               },
-          });              
+          });
+          $.ajax({
+            url:"/admin/graph.php",
+            type :"post",
+            data:{date:asd,
+                  g_type:"rev_bar"},
+            dataType :'json',
+              success: function(data){
+                console.log(data);
+                  var tmp=[]
+                  for(var i=0; i<data.length; i++)
+                  {
+                    tmp.push([data[i]['age_range'],data[i]['cnt']]);
+                  }
+                  console.log(tmp);
+                  google.charts.load('current', {'packages':['corechart']});
+                  google.charts.setOnLoadCallback(drawChart);
+                  function drawChart() {
+                    var data = new google.visualization.DataTable();
+                    data.addColumn('string', 'Age_range');
+                    data.addColumn('number', '개수');
+                    data.addRows(tmp);
+
+                    var options = {
+                      title: '이벤트 TwTwiN'
+                    };
+
+                    var barchart_options = {title:'이벤트: TwTwiN(임시 고정값)',
+                       width:380,
+                       height:300,
+                       legend: 'none'};
+                    var barchart = new google.visualization.BarChart(document.getElementById('barchart_div'));
+                    barchart.draw(data, barchart_options);
+                    }
+              },
+          });
 }
 </script>
 <?php
